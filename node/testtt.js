2ca5bd5a7d9ae5c5a39ba47923c26061be3f8d01
wxy656 <xiaoyiwang860@foxmail.com>
2016-04-22 14:48:23 +0800

    testt

---
+++ node/testtt.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ThinkPad on 2016/3/31.
+ */
+
+song1s = {"ww": 11, "qq": 2}
+songs = [{"ww": 11, "qq": 2}, {"ww": 22, "qq": 22}]
+//favorSimilar = _(songs).map(s=>s["ww"]).flatten().uniq().value();
+for (var i = 0; i < 1111; i++) {
+    a = 10;
+    b = a + b
+    debugger
+    ();
+}
