b91a930c25cfd4cf65d9407574abf4b3603e25eb
wxy656 <xiaoyiwang860@foxmail.com>
2016-04-29 10:45:49 +0800

    test  route

--- node/web_new.js
+++ node/web_new.js
@@ -18,9 +18,9 @@ let render = views('./views/', {
 });
 
 let daySummaryController = require('./day_summary/controller');
-let dingshitask = require('./analysis/cal_everydayData').dingshi;
-let songlistRadioController = require('./everyday_details/s_rController');
-let userListController = require('./everyday_details/userListController');
+//let dingshitask = require('./analysis/cal_everydayData').dingshi;
+//let songlistRadioController = require('./everyday_details/s_rController');
+//let userListController = require('./everyday_details/userListController');
 
 
 
@@ -28,8 +28,8 @@ let userListController = require('./everyday_details/userListController');
 var app=koa();
 app.use(logger());
 app.use(route.get('/day_summary/',daySummaryController.day_summary));
-app.use(route.get('/songlistRadio/',songlistRadioController.songlistRadio));
-app.use(route.get('/userList/',userListController.userList));
+//app.use(route.get('/songlistRadio/',songlistRadioController.songlistRadio));
+//app.use(route.get('/userList/',userListController.userList));
 
 
 app.listen(8040)
